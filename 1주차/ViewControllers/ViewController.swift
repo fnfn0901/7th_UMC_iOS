6acfe92282b10b8e1a77a76e9062c68d66db8f32
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-09-26 16:10:07 +0900

    fix: 숫자 카운트 라벨과 버튼 생성
    - ViewController에서 counterView를 사용하여 카운트 라벨에 "1"이 표시되도록 수정
    - countLabel 및 버튼들의 AutoLayout 설정 완료
    - CountUpDownModel에서 카운트 증가/감소 기능 구현
    - CountUpDownView에 UILabel 및 UIButton 추가

--- 1주차/ViewControllers/ViewController.swift
+++ 1주차/ViewControllers/ViewController.swift
@@ -4,6 +4,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view = CountUpDownView()
+        self.view = counterView
     }
+    
+    private lazy var counterView: CountUpDownView = {
+        let view = CountUpDownView()
+        view.countLabel.text = "1"
+        return view
+    }()
 }
