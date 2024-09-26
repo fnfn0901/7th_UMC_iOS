3c41a31b02b293b3af6fe3f7db5e64829d5f1327
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-09-26 16:14:59 +0900

    fix: Count 증가/감소 기능 및 버튼 액션 추가
    - 증가/감소 버튼을 통해 CountUpDownModel의 카운트 값 변경 기능 구현
    - 버튼에 addTarget을 사용하여 증가, 감소 함수 연결
    - 데이터 변경 시 화면에 업데이트되는 updateCount 함수 추가
    - MVC 패턴 적용하여 뷰와 모델 간의 데이터 연동 및 업데이트 처리

--- 1주차/ViewControllers/ViewController.swift
+++ 1주차/ViewControllers/ViewController.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var data: CountUpDownModel = CountUpDownModel(count: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view = counterView
@@ -9,7 +11,23 @@ class ViewController: UIViewController {
     
     private lazy var counterView: CountUpDownView = {
         let view = CountUpDownView()
-        view.countLabel.text = "1"
+        view.countLabel.text = String(data.count)
+        view.countDownButton.addTarget(self, action: #selector(decreaaseCount), for: .touchUpInside)
+        view.countUpButton.addTarget(self, action: #selector(increaseCount), for: .touchUpInside)
         return view
     }()
+    
+    @objc func increaseCount() {
+        self.data.increaseCount()
+        self.updateCount()
+    }
+    
+    @objc func decreaaseCount() {
+        self.data.decreaseCount()
+        self.updateCount()
+    }
+    
+    private func updateCount() {
+        self.counterView.countLabel.text = String(data.count)
+    }
 }
