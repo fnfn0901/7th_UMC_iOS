4d7bdd7a3bcf703058a30786013751af05d309a3
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-09-30 09:13:59 +0900

    feat: TabBarController 구현하기
    - BaseViewController, ModalViewController, NavigationViewController 생성
    - BaseViewController에 TabBarController 구현

---
+++ KREAM/KREAM/ViewControllers/BaseViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BaseViewController.swift
+//  KREAM
+//
+//  Created by 신호연 on 9/30/24.
+//
+
+import UIKit
+
+class BaseViewController: UITabBarController {
+
+    private let modalVC = ModalViewController()
+    private let navigationVC = NavigationViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        modalVC.tabBarItem = UITabBarItem(title: "modalVC", image: UIImage(systemName: "apple.terminal"), tag: 0)
+        navigationVC.tabBarItem = UITabBarItem(title: "navigationVC", image: UIImage(systemName: "tray.full"), tag: 1)
+        
+        self.viewControllers = [modalVC, navigationVC]
+    }
+    
+
+}
