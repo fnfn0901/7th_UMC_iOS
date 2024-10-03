3ea820bcb33819681500138528f1b87e3dc85d45
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-10-03 13:53:48 +0900

    feat: Controller 구현

--- 3주차/3주차/ViewControllers/ViewController.swift
+++ 3주차/3주차/ViewControllers/ViewController.swift
@@ -8,14 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let userDefaultsModel = UserDefaultsModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view = userDefaultsview
+        loadTextValue()
     }
     
     private lazy var userDefaultsview: UserDefaultsView = {
         let view = UserDefaultsView()
+        view.saveButton.addTarget(self, action: #selector(saveButtonClicked), for: .touchUpInside)
         return view
     }()
+    
+    @objc func saveButtonClicked() {
+        guard let text = userDefaultsview.inputTextField.text, !text.isEmpty else {
+            return
+        }
+        
+        userDefaultsModel.saveUserText(text)
+        
+        userDefaultsview.showTextValue.text = text
+    }
+    
+    private func loadTextValue() {
+        if let savedText = userDefaultsModel.loadUserText() {
+            userDefaultsview.showTextValue.text = savedText
+        }
+    }
 }
