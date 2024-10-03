3b98df29871cdcc248d32f6238ec39cb4290e98b
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-10-03 14:19:08 +0900

    feat: Controller 코드 작성

--- 3주차/Week3TableView/Week3TableView/ViewControllers/DiceViewController.swift
+++ 3주차/Week3TableView/Week3TableView/ViewControllers/DiceViewController.swift
@@ -1,17 +1,35 @@
-//
-//  DiceViewController.swift
-//  Week3TableView
-//
-//  Created by 신호연 on 10/3/24.
-//
-
 import UIKit
 
 class DiceViewController: UIViewController {
     
+    let data = dummyDiceModel.diceDatas
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view = diceView
+    }
+    
+    private lazy var diceView: DiceView = {
+        let view = DiceView()
+        view.tableView.dataSource = self
+        view.tableView.dataSource = self
+        return view
+    }()
+}
+
+
+extension DiceViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: DiceCell.identifier, for: indexPath) as? DiceCell else {
+            return UITableViewCell()
+        }
         
+        cell.configure(model: data[indexPath.row])
         
+        return cell
     }
 }
