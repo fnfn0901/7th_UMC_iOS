0a9f0a0bc8647b0c98c73ca7014cab4651e5950c
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-10-03 14:06:19 +0900

    feat: 모델 코드 작성

--- 3주차/Week3TableView/Week3TableView/Models/DiceModel.swift
+++ 3주차/Week3TableView/Week3TableView/Models/DiceModel.swift
@@ -1,8 +1,16 @@
-//
-//  DiceModel.swift
-//  Week3TableView
-//
-//  Created by 신호연 on 10/3/24.
-//
+struct DiceModel {
+    let diceImage: String
+    let diceName: String
+}
 
-import Foundation
+
+final class dummyDiceModel {
+    static let diceDatas: [DiceModel] = [
+        DiceModel(diceImage: "dice 1.png", diceName: "주사위 1"),
+        DiceModel(diceImage: "dice 2.png", diceName: "주사위 2"),
+        DiceModel(diceImage: "dice 3.png", diceName: "주사위 3"),
+        DiceModel(diceImage: "dice 4.png", diceName: "주사위 4"),
+        DiceModel(diceImage: "dice 5.png", diceName: "주사위 5"),
+        DiceModel(diceImage: "dice 6.png", diceName: "주사위 6")
+    ]
+}
