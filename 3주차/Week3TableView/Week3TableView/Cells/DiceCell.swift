de7f01a3b78d3875b9de2987fa678ba0131b2ef6
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-10-03 14:14:45 +0900

    feat: Cell 코드 작성

--- 3주차/Week3TableView/Week3TableView/Cells/DiceCell.swift
+++ 3주차/Week3TableView/Week3TableView/Cells/DiceCell.swift
@@ -1,8 +1,71 @@
-//
-//  DiceCell.swift
-//  Week3TableView
-//
-//  Created by 신호연 on 10/3/24.
-//
+import UIKit
+import SnapKit
 
-import Foundation
+class DiceCell: UITableViewCell {
+    
+    static let identifier = "DiceCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setViews()
+        self.setConstaints()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.diceImageView.image = nil
+        self.diceName.text = nil
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var diceImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    private lazy var diceName: UILabel = {
+        let name = UILabel()
+        name.font = UIFont.systemFont(ofSize: 20)
+        name.textColor = UIColor.black
+        
+        return name
+    }()
+    
+    private func setViews() {
+        self.addSubview(diceImageView)
+        self.addSubview(diceName)
+    }
+    
+    private func setConstaints() {
+        diceImageView.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(11)
+            $0.left.equalToSuperview().offset(13)
+            $0.bottom.equalToSuperview().offset(-10)
+            $0.width.height.equalTo(75)
+        }
+        
+        diceName.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(36.5)
+            $0.left.equalTo(diceImageView.snp.right).offset(87)
+            $0.right.equalToSuperview().offset(-144)
+        }
+    }
+    
+    public func configure(model: DiceModel) {
+        self.diceImageView.image = UIImage(named: model.diceImage)
+        self.diceName.text = model.diceName
+    }
+}
