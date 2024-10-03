0dd386914b5d60d81d2080ee112aa6c66b846946
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-10-03 12:23:09 +0900

    init: 프로젝트 초기 설정 및 파일 생성

---
+++ 3주차/3주차/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3주차
+//
+//  Created by 신호연 on 10/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
